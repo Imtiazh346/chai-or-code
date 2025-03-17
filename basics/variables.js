44b004005a3d46be13d6e3302737badca2792412
Imtiaz Hussain <97449155+Imtiazh346@users.noreply.github.com>
2025-03-17 16:28:05 +0000

    done

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+// Variables in Javascripts
+const myName = "Imtiaz Hussain" //can't reasign
+let myFather = "Niaz Ahmad" //block/function scope
+var myMother = "Manzooran BB" //can reasign 
+/*Avoid using var use only var and const */
+
+console.log(variables)
