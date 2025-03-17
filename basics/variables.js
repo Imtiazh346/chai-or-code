5c0e4991ffdc225830b2eba24a5abf6637f51684
Imtiaz Hussain <97449155+Imtiazh346@users.noreply.github.com>
2025-03-17 16:35:23 +0000

    variables

--- basics/variables.js
+++ basics/variables.js
@@ -1,7 +1,11 @@
 // Variables in Javascripts
 const myName = "Imtiaz Hussain" //can't reasign
-let myFather = "Niaz Ahmad" //block/function scope
+let myFather = "Niaz Ahmad" //block/function { these curly braces are block } scope
 var myMother = "Manzooran BB" //can reasign 
+cityName = "Lahore"
 /*Avoid using var use only var and const */
 
-console.log(variables)
+console.log(myName, myFather, myMother, cityName) // to print all once in the console
+
+console.table([myName, myFather, myMother, cityName]) // to print in table form 
+
